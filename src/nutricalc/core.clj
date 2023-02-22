7b6caa51eb54d14e137e5584fb77b6a20e6c8dc0
Zach Lott <zacharylott94@gmail.com>
2023-02-22 16:02:27 -0600

    feat: add "add" function

--- src/nutricalc/core.clj
+++ src/nutricalc/core.clj
@@ -1,12 +1,31 @@
 (ns nutricalc.core
   (:gen-class))
+(require '[clojure.string :as string])
+
+
+(defn add 
+  "Adds a csv line to the csv"
+  [entry]
+  (as-> entry x  
+      (rest x)
+      (string/join "," x)
+      (string/join "\n" [x ""])
+      (spit "test.csv" x :append true)
+  )
+  
+)
+
+
+  
+  
+
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (cond 
-    (= (first args) "add") (println "add the thing")
-    :else (println "find the thing"))
-    
+    (= (first args) "add") (add args)
+    :else (-> (slurp "test.csv") println)
+  )
 )
 
