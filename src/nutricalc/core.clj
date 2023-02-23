268f80fd30f54a5e2604d4f7d98330e184d513a7
Zach Lott <zacharylott94@gmail.com>
2023-02-22 21:20:51 -0600

    feat: add ability to fetch lines from CSV

--- src/nutricalc/core.clj
+++ src/nutricalc/core.clj
@@ -7,13 +7,35 @@
   "Adds a csv line to the csv"
   [entry]
   (as-> entry x  
-      (rest x)
       (string/join "," x)
       (string/join "\n" [x ""])
       (spit "test.csv" x :append true)
   )
   
 )
+
+(defn compareFirst
+  "Takes two lists, compares their first items"
+  [lst1 lst2]
+  (= (first lst1) (first lst2))
+
+)
+
+(defn fetch
+  "fetches the requested entry from the CSV file"
+  [args]
+  (-> (slurp "test.csv")
+    (string/split #"\n") ;split file into lines
+    (as-> x 
+      (mapv (fn [x] (string/split x #",")) x)
+      (filterv (partial compareFirst args ) x)
+      (mapv println x) ;; throws out the vector just to print
+      )
+    
+  )
+  
+  
+  )
 
 
   
@@ -24,8 +46,8 @@
   "I don't do a whole lot ... yet."
   [& args]
   (cond 
-    (= (first args) "add") (add args)
-    :else (-> (slurp "test.csv") println)
+    (= (first args) "add") (add (rest args))
+    :else (fetch args)
   )
 )
 
